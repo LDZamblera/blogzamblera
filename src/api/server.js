3d11b4a5aa39f10c88d689e0192b99a99e2968ee
Lucas Daniel Zamblera <129143462+LDZamblera@users.noreply.github.com>
2023-12-07 12:16:05 -0300

    Update server.js
    .

--- src/api/server.js
+++ src/api/server.js
@@ -12,34 +12,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'src')));
 
-// Configuración de CORS
-app.use(
-  cors({
-    origin: origin: 'https://blogzamblera-ldzambleras-projects.vercel.app:3001',// URL del sitio web permitido
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-  })
-);
+app.use(cors({
+  origin: 'https://blogzamblera-ldzambleras-projects.vercel.app:3001',
+  credentials: true,
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+}));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://blogzamblera-kcjafcxfj-ldzambleras-projects.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
-// Rutas de tu aplicación
 const Routes = require("../api/Routes/Routes");
 app.use("/", Routes);
 
-// Conexión a la base de datos y escucha del servidor
 conn.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
   });
 });
+
